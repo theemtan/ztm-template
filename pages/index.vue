b693c54fe7ae5f6e8f7d4a1bdf4d89936addabed
theemtan <minthee.tan@myfave.com>
2022-08-04 01:43:29 +0800

    added another vue file and static folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Planning to create a portfolio of my own</h1>
-      <p class="mt-5">Turning this into a webpage to sell myself.</p>
-      <p class="mt-5">Happy Learning!!!</p>
-      <img src=" https://drive.google.com/uc?export=view&id=12hMAX3qd4nZUdCnLRZPPfIs3TJSr5pJ5" alt="theemtan" width="300" height="200">
-      <a href="https://github.com/theemtan">Here is my Github! (This is a link, will need to turn this into icon) </a>
+  <div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
+    <div id="menu" class="grid grid-cols-2 py-5">
+      <div id="menu_left">
+        <img src="logo.png" class="w-[150px]" />
+      </div>
+
+      <div id="menu_right">
+        <ul class="grid grid-cols-4 text-lg font-papyrus text-white uppercase pt-5">
+          <li>About Us</li>
+          <li>Our Menu</li>
+          <li>Contact Us</li>
+          <li>Location</li>
+        </ul>
+      </div>
+
+      <div class="grid grid-cols-2">
+        <img src="crispy-chicken.jpg" alt="chicken-burger" class="w-[300px] py-10">
+        <img src="double-burger.jpg" alt="double-burger" class="w-[300px] py-10">
+        <ul class="text-lg font-papyrus text-white uppercase pt-5">
+          <li>Cooked with Juicy Chicken!</li>
+        </ul>
+        <ul class="text-lg font-papyrus text-white uppercase pt-5">
+          <li>Cooked With Juicy Beef!</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
---
+++ pages/theemtan.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+    <div class="m-auto">
+      <h1 class="text-5xl">Planning to create a portfolio of my own</h1>
+      <p class="mt-5">Turning this into a webpage to sell myself.</p>
+      <p class="mt-5">Happy Learning!!!</p>
+      <img src=" https://drive.google.com/uc?export=view&id=12hMAX3qd4nZUdCnLRZPPfIs3TJSr5pJ5" alt="theemtan" width="300" height="200">
+      <a href="https://github.com/theemtan">Here is my Github! (This is a link, will need to turn this into icon) </a>
+    </div>
+  </div>
+</template>
